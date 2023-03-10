3c0b106171dbe32df8ebdb35412e8eb40b3065d7
A-n-n-a-P <108946219+A-n-n-a-P@users.noreply.github.com>
2023-03-10 11:12:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fpdf import FPDF
 
+def input(text, position, gorizontal, vertical, size, align):
+    pdf.add_font('DejaVu', '', fname='font\\DejaVuSerif-Italic.ttf')
+    pdf.set_font("DejaVu", size=size)
+    pdf.ln(position)
+    pdf.cell(gorizontal, vertical, txt=text, align=align)
+
+
+
 pdf = FPDF()
 pdf.add_page()
-pdf.add_font('DejaVu', '',fname='font\\DejaVuSerif-Italic.ttf', uni=True)
-pdf.set_font("DejaVu", size=30)
-pdf.cell(200, 10, txt="СЕРТИФИКАТ", ln=1, align="C")
+
+ussr="СССР"
+diplom="Диплом"
+degree="Ⅰ степени"
+name="Юрий Алексеевич"
+lastName="Гагарин"
+
+
+
+input(ussr, 23, 180, 10, 60, "C")
+input(diplom, 23, 180, 10, 60, "C")
+input(degree, 23, 180, 10, 60, "C")
+input(ussr, 23, 180, 10, 60, "C")
+#pdf.image('D:\\Popova\\pdf\\hedgehog.jpg', x=10, y=10, w=100)
+
+
+
 pdf.output("D:\\Popova\\pdf\\Certificate.pdf")
